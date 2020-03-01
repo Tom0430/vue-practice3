c5286117fd6a11c741629c42313c23c350ac0375
tomoya takahashi <tiger@tomoyanoMacBook-Pro.local>
2020-03-01 23:49:45 +0900

    axiosで取れたデータを画面に表示

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -4,11 +4,11 @@
     <h1>商品情報</h1>
     <dl class="product-table">
       <dt>商品名</dt>
-      <dd>{{ item.name }}</dd>
+      <dd>{{ item[0].name }}</dd>
       <dt>価格</dt>
-      <dd>{{ item.price }}円</dd>
+      <dd>{{ item[0].price }}円</dd>
       <dt>商品説明</dt>
-      <dd>{{ item.content }}</dd>
+      <dd>{{ item[0].content }}</dd>
     </dl>
   </div>
   <div v-else key="loading">商品情報を読み込んでいます...</div>
@@ -18,24 +18,41 @@
 <script>
   import products from '@/api/products.js'
   export default {
-    props: {
-      id: Number
-    },
     data() {
-      return {
-        item: null
+          return {
+              item: null
       }
     },
-    watch: {
-      id: {
-        handler() {
-          products.asyncFind(this.id, item => {
-            this.item = item
-          })
-        },
-        immediate: true
-        //immediateは、変更だけでなく初期化時にもイベンんとを発生させたい場合はtrueにします。
-      }
+    props: {
+        id: Number,
+    },
+    created (id){
+        this.axios.get('https://api.myjson.com/bins/idr9w')
+        .then((response) => {
+            let items = response.data
+            console.log(this.item)
+            this.item = items.filter((itm) => {
+                console.log(itm)
+                console.log(itm.id)
+                console.log("this.id" , this.id)
+                return parseInt(itm.id) === this.id
+            })
+            console.log(this.item[0].id)
+        })
+        // .catch((e) => {
+        //     alert(e);
+        // });
     }
+//     watch: {
+//       id: {
+//         handler() {
+//           products.asyncFind(this.id, item => {
+//             this.item = item
+//           })
+//         },
+//         immediate: true
+//         //immediateは、変更だけでなく初期化時にもイベンんとを発生させたい場合はtrueにします。
+//       }
+//     }
   }
 </script>
--- src/pages/ProductList.vue
+++ src/pages/ProductList.vue
@@ -3,31 +3,29 @@
     <h1>商品一覧</h1>
     <ul>
       <li v-for="{ id, name } in list" :key="id">
-        <router-link :to="`/product/${ id }`">{{ name }}</router-link>
+        <router-link :to="`/product/${ id }`" :id="id">{{ name }}</router-link>
       </li>
-      <div @click="getIp">商品一覧</div>
+
     </ul>
   </div>
 </template>
 
 <script>
-  export default {
-    data(){
-        return {
-            list: null
-        }
-    },
-    methods: {
-        getIp() {
-            this.axios.get('https://api.myjson.com/bins/idr9w')
-            .then((response) => {
-                console.log(response.data);
-                this.list = response.data
-            })
-            .catch((e) => {
-                alert(e);
-            });
-        }
+export default {
+data(){
+    return {
+        list: null
     }
-  }
+},
+
+created (){ //ここの書き方は覚えてしまっておk
+        this.axios.get('https://api.myjson.com/bins/idr9w')
+        .then((response) => {
+            this.list = response.data
+        })
+        .catch((e) => {
+            alert(e);
+        });
+    }
+}
 </script>
